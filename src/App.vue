7ac5a70e0026a31de9b64c379c9c6f7f0d1c5686
codebender828 <jbakebwa@163.com>
2023-02-26 01:19:39 +0800

    feat: build clean uis

--- src/App.vue
+++ src/App.vue
@@ -23,9 +23,10 @@
         <c-heading as="h1" text-style="home.heading"> Chakra UI Vue </c-heading>
         <chakra.h4
           font-weight="bold"
-          font-size="3xl"
+          :font-size="['xl', '2xl', '3xl']"
           display="flex"
-          align-items="center"
+          :flex-direction="['column', 'column', 'row']"
+          :align-items="['flex-start', 'flex-start', 'center']"
         >
           <chakra.span
             >Build Beautiful Accessible Components - Powered by</chakra.span
@@ -35,54 +36,71 @@
             text-decoration="underline"
             text-decoration-style="dashed"
             cursor="pointer"
-            href="https://vuejs.org"
+            href="https://nuxt.com"
             target="_blank"
             display="inline-flex"
             justify-content="flex-start"
             align-items="center"
           >
-            Vue.js &nbsp;
             <VueLogo h="1em" d="inline-flex" />
             <ExternalLinkIcon />
           </chakra.a>
         </chakra.h4>
       </c-stack>
-      <c-h-stack
+      <c-wrap
         :bg="useColorModeValue('blackAlpha.50', 'whiteAlpha.50').value"
-        padding="3"
-        rounded="full"
+        :px="[5, 3]"
+        :py="[4, 3]"
+        :rounded="{ base: '2xl', md: '2xl', lg: 'full' }"
+        spacing="3"
       >
-        <c-circle
-          box-size="8"
-          v-for="color in colors"
-          :key="color"
-          :bg="`${color}.400`"
-          cursor="pointer"
-          :_hover="{
-            bg: `${color}.500`,
-            transform: 'scale(1.1)',
-            transition: 'all 0.2s',
-          }"
-          @mouseenter="selectedColor.color = color"
-        />
-      </c-h-stack>
+        <c-wrap-item v-for="color in colors" :key="color">
+          <c-circle
+            box-size="8"
+            :bg="`${color}.400`"
+            cursor="pointer"
+            :_hover="{
+              bg: `${color}.500`,
+              transform: 'scale(1.1)',
+              transition: 'all 0.2s',
+            }"
+            @mouseenter="selectedColor.color = color"
+          />
+        </c-wrap-item>
+      </c-wrap>
       <c-badge
-        rounded="full"
         :color-scheme="selectedColor.color"
         text-transform="unset"
-        px="2"
-        py="1"
+        :px="[5, 2]"
+        :py="[4, 1]"
         font-size="md"
+        :rounded="{ base: '2xl', md: '2xl', lg: 'full' }"
+        white-space="break-spaces"
       >
         Chakra UI Vue is currently still in early beta. Explore the
         documentation and join our Discord!
       </c-badge>
-      <c-button-group size="lg" spacing="5" :color-scheme="selectedColor.color">
-        <c-button :right-icon="ExternalLinkIcon"> Get Started </c-button>
-        <c-button :right-icon="DocumentationIcon" variant="outline">
-          Documentation
-        </c-button>
-      </c-button-group>
+      <c-wrap spacing="5" :color-scheme="selectedColor.color">
+        <c-wrap-item>
+          <c-button
+            size="lg"
+            :color-scheme="selectedColor.color"
+            :right-icon="ExternalLinkIcon"
+          >
+            Get Started
+          </c-button>
+        </c-wrap-item>
+        <c-wrap-item>
+          <c-button
+            size="lg"
+            :color-scheme="selectedColor.color"
+            :right-icon="DocumentationIcon"
+            variant="outline"
+          >
+            Documentation
+          </c-button>
+        </c-wrap-item>
+      </c-wrap>
       <c-button-group :color-scheme="selectedColor.color" variant="outline">
         <c-icon-button
           aria-label="Toggle color mode"
@@ -92,7 +110,7 @@
         />
         <c-icon-button
           as="a"
-          href="https://vuejs.org"
+          href="https://nuxt.com"
           target="_blank"
           aria-label="Join Discord"
           rounded="none"
@@ -105,16 +123,14 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
 import {
   chakra,
   useColorMode,
   useColorModeValue,
   useTheme,
-  canUseDOM,
 } from "@chakra-ui/vue-next";
+import { computed } from "vue";
 import { useStorage } from "@vueuse/core";
-
 import DocumentationIcon from "./components/icons/documentation-icon";
 import ExternalLinkIcon from "./components/icons/external-link";
 import DiscordIcon from "./components/icons/discord-icon";
@@ -123,10 +139,7 @@ import VueLogo from "./components/icons/vue-logo";
 const { toggleColorMode } = useColorMode();
 
 const themeConfig = {
-  color: canUseDOM()
-    ? JSON.parse(localStorage.getItem("theme.color")! || '{"color":"whatsapp"}')
-        .color
-    : "whatsapp",
+  color: "whatsapp",
 };
 const selectedColor = useStorage("theme.color", themeConfig);
 
